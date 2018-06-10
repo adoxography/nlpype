d55e262aef5c1e1ba762e481ae81727a8c6782e6
Adoxography <gstill@uw.edu>
2018-06-09 22:09:42 -0700

    Add NER tagging function

---
+++ nlpype/ner.py
@@ -0,0 +1,11 @@
+from nlpype.annotators import ENTITY_MENTIONS
+from nlpype.output import NERPresenter
+
+
+def tag_named_entities(text, parser):
+    if ENTITY_MENTIONS not in parser.annotators:
+        raise AttributeError('The parser was not initalized with the entity mentions annotator.')
+    presenter = NERPresenter()
+    document = parser.annotate(text)
+    return presenter.convert(document)
+
